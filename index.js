b329525e26dd59e64b194732849769ea45f6d3b6
Marcus Wagner <marcus@schmidt-sound.de>
2019-07-18 13:17:01 +0200

    proxy middleware disabled

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ let projects = JSON.parse(fs.readFileSync(__dirname + '/src/db.json'))
 
 console.log('prod', process.env.NODE_ENV)
 
-if (process.env.NODE_ENV != 'production') {
-    app.use(
-        '/bundle.js',
-        require('http-proxy-middleware')({
-            target: 'http://localhost:8081/'
-        })
-        );
-} else {
-    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`))
-}   
+// if (process.env.NODE_ENV != 'production') {
+//     app.use(
+//         '/bundle.js',
+//         require('http-proxy-middleware')({
+//             target: 'http://localhost:8081/'
+//         })
+//         );
+// } else {
+//     app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`))
+// }   
 
 app.use(express.static(__dirname + '/public'))
     
